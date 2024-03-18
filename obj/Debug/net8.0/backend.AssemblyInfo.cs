f7bfd3723e6969007dda1f932e4df5080db66466
Henrique Onorato <onoratok2@gmail.com>
2024-03-18 20:18:42 -0300

    feat: arquivos back

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ed5c007018e32c85990d97424df63d8d7927c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d58b26518338ace21b543916210b3c6471027c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
