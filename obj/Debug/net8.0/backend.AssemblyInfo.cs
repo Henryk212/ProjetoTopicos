9c704ebcdd258b87ad6a03c1131c882085d42621
Henrique Onorato <onoratok2@gmail.com>
2024-03-18 21:43:33 -0300

    iniciando com API's

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d58b26518338ace21b543916210b3c6471027c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7bfd3723e6969007dda1f932e4df5080db66466")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
